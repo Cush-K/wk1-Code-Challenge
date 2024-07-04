5d86f40d38b6a1bf9e2cdc082fb7337ee20006a8
Kalvine <kkalvine4@gmail.com>
2024-07-05 01:26:11 +0300

    Adds NHIF, NSSF, and Housing Levy to Calculations

--- challenge3.js
+++ challenge3.js
@@ -2,14 +2,19 @@ function paye(salary) {
     const second = 8333; //range betweeen 24,000 & 32,333
     const third = 467667; //Range between 32,333 & 500,000
     const fourth = 300000; // Range between 500,000 & 800,000
-    const nssf = (.06 * salary); //6% NSSF calculation
-    const taxablePay = (salary - nssf);
+    function nssf (salary){if(salary>0 && salary <= 36000){
+        return .06*salary
+    }else{
+       return 2160;
+    }
+    }
+    const nssfAmount = nssf(salary)
+    const taxablePay = (salary - nssfAmount);
     const tierTwo = (taxablePay - 24000); //Tier with 25% taxation
     const tierThree = (tierTwo - second); //Tier with 30% taxation
     const tierFour = (tierThree - third); //Tier with 32.5% taxation
     const tierFive = (tierFour - fourth); //Tier with 35% taxation
-    let range1A = (taxablePay * .1) //tax on taxable pay less than 24,000
-    let range1B = (24000 * .1) //maximum taxable amount for range 0 to 24,000
+    let range1 = (24000 * .1); //maximum taxable amount for range 0 to 24,000
     let range2A = (tierTwo * .25); //tax on taxable pay less than "second" above
     let range2B = second * .25; //maximum taxable amount for "second" above
     let range3A = (tierThree * .3); //tax on taxable pay less than "third" above
@@ -17,35 +22,108 @@ function paye(salary) {
     let range4A = (tierFour * .325); //tax on taxable pay less than "fourth" above
     let range4B = fourth * .325;//maximum taxable amount for "fourth" above
     let range5 = tierFive * .35; //tax on taxable pay greater than 800,000
-    let tax2 = (range1B + range2A);
-    let tax3 = (range1B + range2B);
+    let tax2 = (range1 + range2A);
+    let tax3 = (range1 + range2B);
     let tax4 = (tax3 + range3A);
     let tax5 = (tax3 + range3B);
     let tax6 = (tax5 + range4A);
     let tax7 = (tax5 + range4B);
     let tax8 = (tax7 + range5);
 
-    console.log(`Taxable Pay:` + taxablePay);
+    // console.log(`Taxable Pay:` + taxablePay);
+    
 
 
     if (taxablePay > 0 && taxablePay < 24000) {
-        return range1A;
+        return 0;
     } else if (taxablePay === 24000) {
-        return range1B;
+        return (range1-2400);
     }else if (tierTwo <= second) {
-        return tax2;
+        return (tax2-2400);
     }else if (second > tierTwo) {
-        return tax3
+        return (tax3-2400);
     }else if (tierThree <= third) {
-        return tax4
+        return (tax4-2400);
     }else if (third > tierThree) {
-        return tax5;
+        return (tax5-2400);
     }else if (tierFour <= fourth) {
-        return tax6;
+        return (tax6-2400);
     }else if (fourth > tierFour) {
-        return tax7;
+        return (tax7-2400);
     }else if (tierFive > 0) {
-        return tax8;
+        return (tax8-2400);
+    }
+    return 
+}
+// console.log(`Tax After Relief: ` + paye(30000));
+
+
+function nhif (salary){
+    if (salary >0 && salary <= 5999){
+        return 150;
+    }else if(salary >5999 && salary <= 7999){
+        return 300;
+    }else if(salary >7999 && salary <= 11999){
+        return 400;
+    }else if(salary >11999 && salary <= 14999){
+        return 500;
+    }else if(salary >14999 && salary <= 19999){
+        return 600;
+    }else if(salary >19999 && salary <= 24999){
+        return 750;
+    }else if(salary >24999 && salary <= 29999){
+        return 850;
+    }else if(salary >29999 && salary <= 34999){
+        return 900;
+    }else if(salary >34999 && salary <= 39999){
+        return 950;
+    }else if(salary >39999 && salary <= 44999){
+        return 1000;
+    }else if(salary >44999 && salary <= 49999){
+        return 1100;
+    }else if(salary >49999 && salary <= 59999){
+        return 1200;
+    }else if(salary >59999 && salary <= 69999){
+        return 1300;
+    }else if(salary >69999 && salary <= 79999){
+        return 1400;
+    }else if(salary >79999 && salary <= 89999){
+        return 1500;
+    }else if(salary >89999 && salary <= 99999){
+        return 1600;
+    }else{
+        return 1700;
     }
 }
-console.log(`Tax Before Relief:` + paye(700000));
+
+// console.log(`NHIF:` + nhif(500000));
+
+
+function netSalary(sal){
+    function nssf (salary){if(salary>0 && salary <= 36000){
+        return .06*salary
+    }else{
+       return 2160;
+    }
+    }
+    const nssfAmount = nssf(sal)
+    const nhifAmount = nhif(sal);
+    const housingLevy = (.015*sal);
+    const housingRelief = Math.ceil((.15*housingLevy));
+    const nhifRelief = Math.ceil((.15*nhifAmount));
+    const payeAmount = (paye(sal)-(housingRelief+nhifRelief));
+    const net = sal - (payeAmount + nhifAmount + housingLevy+nssfAmount)
+    
+console.log(`Paye ` +payeAmount);
+console.log(`NHIF ` +nhifAmount);
+console.log(`H.Levy ` +housingLevy);
+console.log(`H.Relief ` +housingRelief);
+console.log(`NHIF.R ` +nhifRelief);
+console.log(`NSSf ` +nssfAmount)
+console.log(`Taxable Pay:` + `${sal-nssfAmount}`);
+
+    return net;
+}
+
+
+console.log(`Your Net Salary: ` +netSalary(70000));
