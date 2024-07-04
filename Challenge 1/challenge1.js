84c592e2d87ef213dc573a1b9e4fa9aae5fc0b6b
Kalvine <kkalvine4@gmail.com>
2024-07-05 02:14:38 +0300

    Adds prompt and HTML file

--- Challenge 1/challenge1.js
+++ Challenge 1/challenge1.js
@@ -3,28 +3,28 @@
 // input to correspond with correct grade
 // A > 79, B - 60 to 79, C - 59 to 49, D - 40 to 49, E < 40
 
-
+let grades = prompt(`Please enter the Student's Grade`)
 function grading(grade){
     
     switch(true){
         case grade>=79 && grade<=100:
-            console.log(`A`);
+            return `${grade} is an A`;
         break;
         case grade>=60 && grade<79:
-            console.log(`B`)
+            return `${grade} is a B`
         break;
         case grade>=50 && grade<60:
-            console.log(`C`)
+            return `${grade} is a C`
         break;
         case grade>=40 && grade<50:
-            console.log(`D`)
+            return `${grade} is a D`
         break;
         case grade>=0 && grade<40:
-            console.log(`E`)
+            return `${grade} is an E`
         break;
         default:
-            console.log(`Please enter a number between 0 and 100`);
+            return `Please enter a number between 0 and 100`;
     }
     
 }
-    grading();
+    alert(grading(grades));
